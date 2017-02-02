c908976ccee0c75ea87c18126c629e71b4a4aa05
vogtn <nicolasjvogt@gmail.com>
2017-02-02 08:53:29 -0800

    adding transitions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StackGrid, { transitions } from "react-stack-grid";
+
+const { scaleDown } = transitions;
 
 class App extends Component {
   render() {
@@ -13,6 +16,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <StackGrid
+          appear={scaleDown.appear}
+          appeared={scaleDown.appeared}
+          enter={scaleDown.enter}
+          entered={scaleDown.entered}
+          leaved={scaleDown.leaved}
+          columnWidth={150}>
+        <div key="key1">Item 1</div>
+        <div key="key2">Item 2</div>
+        <div key="key3">Item 3</div>
+        </StackGrid>
       </div>
     );
   }
